91ce39c3f6eeb22ff7c385b0f7a57c6380eb1e27
HomeDev <nhatv933@gmail.com>
2025-05-13 10:54:46 +0700

    add some feature

--- js/home.js
+++ js/home.js
@@ -1,4 +1,10 @@
 const cardGroups = document.querySelectorAll('.section10-cards');
+const seeMore = document.querySelector('#seeMoreFea')
+seeMore.addEventListener('click',(e)=>{
+  e.preventDefault()
+  const item = document.querySelector(".addedItem")
+   item.style.display = (getComputedStyle(item).display === "none") ? "block" : "none";
+})
 
 cardGroups.forEach(group => {
   const cards = group.querySelectorAll('.section10-card');
